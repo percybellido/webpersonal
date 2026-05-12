4f0853bb0c849f59b35f24d80b727e5528ae8e3c
Percy Bellido <pbellido0401@gmail.com>
2026-05-11 21:47:45 -0500

    agregando STATIC_ROOT

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 #Media files
 MEDIA_URL='/media/'
 MEDIA_ROOT = BASE_DIR / "media"
